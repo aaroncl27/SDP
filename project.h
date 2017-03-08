53feff1828f6de775e4fc0e4ec1ce0a7d4cbbbd5
aaroncl27 <acl27@zips.uakron.edu>
2017-03-08 18:57:00 -0500

    added some variables, changed pinOut numbers

--- project.h
+++ project.h
@@ -10,23 +10,17 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define Timer 22
-#define TempSet1 23
-#define TempSet2 24
-#define relayPin 13
-#define buzzPin 3
-#define Temp1 A0
-#define Temp2 A2
-#define Temp3 A4
-#define Temp4 A6
-#define Temp5 A8
 #define ONE_WIRE_BUS 2
-#define ckt 11
-#define TempPin1 12 // assign pin 12 to High Temp
-#define TempPin2 10// assign pin 13 to Medium Temp
-#define TimePin1  5 // assign pin 5 to 15 Minutes
-
+#define BuzzPin 3
 #define OLED_RESET 4
+#define ckt 11
+#define Timer 22
+#define TimePin  24 // assign pin 5 to 15 Minutes
+#define TempPin2 28// assign pin 13 to Medium Temp
+#define TempPin1 32 // assign pin 12 to High Temp
+#define RelayPin 31
+#define Button 51
+
 Adafruit_SSD1306 display(OLED_RESET);
 
 // Setup a oneWire instance to communicate with any OneWire devices
@@ -35,5 +29,21 @@ OneWire oneWire(ONE_WIRE_BUS);
 
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
+
+
+//Declare variables for runtime and operating temperature
+int SetTempConst = 38,SetTemp, temptest, timeDisp;
+unsigned long SetTimeConst=0, SetTime=1, runTime;
+//  int SetTemp, SetTime;
+// Variables will change:
+int ButtonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 200;    // the debounce time; increase if the output
+
+
+
+
+
 
 #endif // PROJECT_H_INCLUDED
