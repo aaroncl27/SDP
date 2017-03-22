617335693ebd5971a41be10818755579aca3a8f5
aaroncl27 <acl27@zips.uakron.edu>
2017-03-22 19:19:14 -0400

    3/22/17 Commit
    Implemented Frequency Relay Code. Minor code cleaning

--- Final_Code/Final_Code.ino
+++ Final_Code/Final_Code.ino
@@ -15,26 +15,26 @@ void setup() {
   pinMode(TempPin2, INPUT);
   pinMode(TimePin, INPUT);
   pinMode(Button, INPUT);
+  pinMode(CoilRelayPin, OUTPUT);
   digitalWrite(Button, LOW);
   digitalWrite(RelayPin, LOW);
+  digitalWrite(CoilRelayPin, LOW);
   InitTimersSafe(); //initialize all timers except for 0, to save time keeping functions
-
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // initialize with the I2C addr 0x3C
   display.display();
   delay(250);
   display.clearDisplay();
-
   sensors.begin();
   Init();
-  //setFreq();
+
 }
 
-void (* resetFunc)(void)=0;
+void (* resetFunc)(void) = 0;
 //Get Operating Temperature and Runtime Settings
 
 void Init() {
   if (digitalRead(TimePin) == HIGH) {
-   SetTime = 900000; //15 desired minutes in milliseconds
+    SetTime = 900000; //15 desired minutes in milliseconds
   }
   else {
     SetTime = 1800000; //30 desired minutes in milliseconds
@@ -51,7 +51,7 @@ void Init() {
     SetTemp = 36; //Sets desired temp to 36C
   }
   Serial.print("Device Runtime: ");
-  Serial.println(SetTime/60/1000);//Display Desired Runtime
+  Serial.println(SetTime / 60 / 1000); //Display Desired Runtime
   Serial.print("Device Temperature Setpoint: ");
   Serial.println(SetTemp);//Display Desired Operating Temperature
 
@@ -63,31 +63,56 @@ void tempCheck() {
   if (temp >= 40) {
     pulseOff();//If the temperature read by the device is over 40C, kill power to the device
   }
+
+  if ((temp < SetTemp - 0.5) && (RelayState == FALSE)) {
+    digitalWrite(CoilRelayPin, HIGH);
+    delay(20);
+    digitalWrite(CoilRelayPin, LOW);
+    delay(1000);
+    RelayState = TRUE;
+  }
+
+  if ((temp >= SetTemp + 0.5) && (RelayState == TRUE)) {
+    digitalWrite(CoilRelayPin, HIGH);
+    delay(20);
+    digitalWrite(CoilRelayPin, LOW);
+    delay(1000);
+    RelayState = FALSE;
+  }
+
 }
 
 float Temp() {
-  float temp1, temp2, temp3, temp4, temp5, avgTemp;
+  float temp1, temp2, temp3, temp4, temp5, avgTemp, compAvgTemp;
   int devCount, devCountAdj;
-  
-  
+
   sensors.requestTemperatures(); // Send the command to get temperatures
-  devCount=sensors.getDeviceCount();
-  devCountAdj=devCount;
+  devCount = sensors.getDeviceCount();
+  devCountAdj = devCount;
   float temp[devCount];
-  
-  for(int j=0;j<devCount;j++){
-  temp[j]=sensors.getTempCByIndex(j);
+  compAvgTemp = sensors.getTempCByIndex(2);
+  for (int j = 0; j < devCount; j++) {
+    temp[j] = sensors.getTempCByIndex(j);
   }
-  for (int j = 0;j<devCount;j++){
-    if(temp[j]>0){
-    avgTemp += temp[j];}
-    else{
+  for (int j = 0; j < devCount; j++) {
+    if ((temp[j] < compAvgTemp - 2)) {
+      temp[j] = -127;
+      Serial.println(temp[j]);
+    }
+  }
+  for (int j = 0; j < devCount; j++) {
+    if (temp[j] > 0) {
+      avgTemp += temp[j];
+    }
+    else {
       devCountAdj--;
     }
   }
   avgTemp = avgTemp / devCountAdj;
-  
-  Serial.print("Temperature Average is: ");
+  compAvgTemp = avgTemp;
+  Serial.print("Desired Temp: ");
+  Serial.println(SetTemp);
+  Serial.print("Average Temp: ");
   Serial.println(avgTemp);
   Display(avgTemp);
   return avgTemp;
@@ -103,14 +128,11 @@ void Display(float tempReadout) {
   display.setTextSize(2);
   display.println("Temp ");
   //Serial.println(Temp());
-  Serial.print("Thermistor Avg: ");
-  Serial.println(tempReadout);
   display.print(tempReadout);
   display.println(" C");
   display.print(timeDisp);
   display.println(" Min Rem");
   display.display();
-  //delay(250);
 
 }
 
@@ -121,16 +143,11 @@ void warningBeep() {
   delay(500);
 }
 
-bool startButton(){
+bool startButton() {
   ButtonState = LOW;
-    // read the state of the switch into a local variable:
+  // read the state of the switch into a local variable:
   int reading = digitalRead(Button);
 
-  // check to see if you just pressed the Button
-  // (i.e. the input went from LOW to HIGH),  and you've waited
-  // long enough since the last press to ignore any noise:
-
-  // If the switch changed, due to noise or pressing:
   if (reading != lastButtonState) {
     // reset the debouncing timer
     lastDebounceTime = millis();
@@ -145,82 +162,56 @@ bool startButton(){
       ButtonState = reading;
     }
   }
-    lastButtonState = reading;
+  lastButtonState = reading;
   return ButtonState;
 }
 
 void pulseOff() {
-//tie an IO pin with a 1k resistor to the reset pin
-//when pin is low it isnt reset
-//when pin is high the arduino resets
-digitalWrite(LedPin, LOW);
+  //tie an IO pin with a 1k resistor to the reset pin
+  //when pin is low it isnt reset
+  //when pin is high the arduino resets
+  digitalWrite(LedPin, LOW);
   digitalWrite(RelayPin, LOW);//Brings pin tied to the latch on the relay to low
   delay(50);//waits for 20 ms
   digitalWrite(RelayPin, HIGH);//Brings pin tied to the relay to high, flipping the latch on the relay
   delay(5000);
-    resetFunc();
+  resetFunc();
 
 }
 
-/*void demonstrateFrequencysEffectOnResolution()
-  {
-  Serial.println("As frequency increases, resolution will decrease...");
-  for (int i = 1; i < 10000; i += 10)
-  {
-    SetPinFrequency(ckt, i);  //setting the frequency
-    uint16_t frequency = Timer1_GetFrequency();
-    uint16_t decimalResolution = Timer1_GetTop() + 1;
-    uint16_t binaryResolution = GetPinResolution(ckt); //this number will be inaccurately low because the float is being truncated to a int
-    char strOut[75];
-    sprintf(strOut, "Frequency: %u Hz\r\n Number of Possible Duties: %u\r\n Resolution: %u bit\r\n", frequency, decimalResolution, binaryResolution );
-    Serial.println(strOut);
-  }
-  Serial.println("...Finished");
-  }*/
-
-void setFreq()
+void Main()
 {
   int k = 0;
   bool estop;
-  SetPinFrequency(ckt, 10); //setting the frequency to 10 Hz
-  //Serial.println("\r\npwmWrite() and pwmWriteHR() are identical except for the valid range of inputs.\r\nThe following loop calls both functions to produce the same result on the \r\nLED pin. The pin should to run 10Hz at 50% duty regardless of the function called.\r\n");
-  //(10Hz 50% duty) for 1 second before calling
-  //the other function. This demonstrates the use of pwmWriteHR() and how its
-  //use is nearly identical to pwmWrite()
   while (true)
   {
-    //if(startButton() == HIGH){
-    //  pulseOff();
-    //}
-    //setting the duty to 50% with 8 bit pwm. 128 is 1/2 of 256
     estop = startButton();
-    pwmWrite(ckt, 128);
-    //Serial.println("8-Bit PWM");
-
-    //setting the duty to 50% with the highest possible resolution that
-    //can be applied to the timer (up to 16 bit). 1/2 of 65536 is 32768.
-    //pwmWriteHR(ckt, 32768);
-    //Serial.println("High Resolution PWM");
-    //delay(1000);
     if (millis() % 1000 == 0) {
       Serial.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
       tempCheck();
-      if((digitalRead(TimePin)==HIGH&&SetTime!=900000)||((digitalRead(TimePin)==LOW&&SetTime!=1800000))){
-      delay(100);
-      Init();}
-      if((digitalRead(TempPin1)==HIGH&&SetTemp!=38)||((digitalRead(TempPin2)==HIGH&&SetTemp!=34))||(((digitalRead(TempPin1)==LOW&&digitalRead(TempPin2)==LOW)&&SetTemp!=36))){
-      delay(250);
-      Init();}
-      runTime = millis()-StandbyTime;
+      if ((digitalRead(TimePin) == HIGH && SetTime != 900000) || ((digitalRead(TimePin) == LOW && SetTime != 1800000))) {
+        delay(100);
+        Init();
+      }
+      if ((digitalRead(TempPin1) == HIGH && SetTemp != 38) || ((digitalRead(TempPin2) == HIGH && SetTemp != 34)) || (((digitalRead(TempPin1) == LOW && digitalRead(TempPin2) == LOW) && SetTemp != 36))) {
+        delay(250);
+        Init();
+      }
+      runTime = millis() - StandbyTime;
       Serial.print("Runtime in seconds: ");
       Serial.println(runTime / 1000);
-      Serial.print("Runtime in ms: ");
-      Serial.println(millis()-748);
+      //Serial.print("Runtime in ms: ");
+      //Serial.println(millis() - 748);
       Serial.println(startButton());
+      //Serial.println(sensors.getTempCByIndex(0));
+      //Serial.println(sensors.getTempCByIndex(1));
+      //Serial.println(sensors.getTempCByIndex(2));
+      //Serial.println(sensors.getTempCByIndex(3));
+      //Serial.println(sensors.getTempCByIndex(4));
       Serial.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
       timeDisp = (((SetTime / 1000) / 60) - ((runTime / 1000) / 60));//compare the time in min to the runtime in min.
 
-      if (timeDisp <= 0||estop==HIGH){//when the timer on the device reaches zero
+      if (timeDisp <= 0 || estop == HIGH) { //when the timer on the device reaches zero
         display.clearDisplay();
         display.setTextSize(2);//Set Text Size to one for the top line. This allows 2 lines of text to fit. Otherwise text size should be 2
         display.setTextColor(WHITE);
@@ -230,70 +221,65 @@ void setFreq()
         display.print("  Heating");
         display.display();
         for (int n = 0; n < 3; n++) {
-          //warningBeep();
+          //uwarningBeep();
         }
         //while (1) {} //remove this when no longer testing
         delay(500);
         pulseOff();
-       }
+      }
     }
   }
 }
 
-void initDisplay(){
+void initDisplay() {
   display.clearDisplay();
-        display.setTextSize(2);//Set Text Size to one for the top line. This allows 2 lines of text to fit. Otherwise text size should be 2
-        display.setTextColor(WHITE);
-        display.setCursor(0, 0);
-        display.println();
-        display.println("  Initial");
-        display.println("   Setup");
-        display.println("    .");
-        display.display();
-        delay(1000);
-        display.clearDisplay();
-        display.setTextSize(2);//Set Text Size to one for the top line. This allows 2 lines of text to fit. Otherwise text size should be 2
-        display.setTextColor(WHITE);
-        display.setCursor(0, 0);
-        display.println();
-        display.println("  Initial");
-        display.println("   Setup");
-        display.println("    ..");
-        display.display();
-        delay(1000);
-        display.clearDisplay();
-        display.setTextSize(2);//Set Text Size to one for the top line. This allows 2 lines of text to fit. Otherwise text size should be 2
-        display.setTextColor(WHITE);
-        display.setCursor(0, 0);
-        display.println();
-        display.println("  Initial");
-        display.println("   Setup");
-        display.println("    ...");
-        display.display();
-        delay(1000);
-        display.display();
+  display.setTextSize(2);//Set Text Size to one for the top line
+  display.setTextColor(WHITE);//Set the text color to white
+  display.setCursor(0, 0);//Set cursor on first line of the screen to start printing
+  display.println();//Leaves top line blank
+  display.println("  Initial");
+  display.println("   Setup");
+  display.println("    .");
+  display.display();
+  delay(1000);
+  display.clearDisplay();
+  display.setTextSize(2);//Set Text Size to one for the top line
+  display.setTextColor(WHITE);//Set the text color to white
+  display.setCursor(0, 0);//Set cursor on first line of the screen to start printing
+  display.println();//Leaves top line blank
+  display.println("  Initial");
+  display.println("   Setup");
+  display.println("    ..");
+  display.display();
+  delay(1000);
+  display.clearDisplay();
+  display.setTextSize(2);//Set Text Size to one for the top line
+  display.setTextColor(WHITE);//Set the text color to white
+  display.setCursor(0, 0);//Set cursor on first line of the screen to start printing
+  display.println();//Leaves top line blank
+  display.println("  Initial");
+  display.println("   Setup");
+  display.println("    ...");
+  display.display();
 }
 
-void loop () {  
+void loop () {
   display.clearDisplay();
-        display.setTextSize(3);//Set Text Size to one for the top line. This allows 2 lines of text to fit. Otherwise text size should be 2
-        display.setTextColor(WHITE);
-        display.setCursor(0, 0);
-                display.display();
- StandbyTime = millis();
-      if (startButton() == HIGH) {
-        digitalWrite(LedPin, HIGH);
-        initDisplay();
-        
-        digitalWrite(RelayPin, LOW);//Brings pin tied to the latch on the relay to low
-          delay(50);//waits for 20 ms
-          digitalWrite(RelayPin, HIGH);//Brings pin tied to the relay to high, flipping the latch on the relay
+  display.setTextSize(3);//Set Text Size to one for the top line
+  display.setTextColor(WHITE);//Set the text color to white
+  display.setCursor(0, 0);//Set cursor on first line of the screen to start printing
+  display.println();//Leaves top line blank
+    display.display();
+  StandbyTime = millis();//Keep track of how long the device waits for the user to press the button. This is used for calculating the runtime of the device.
+  if (startButton() == HIGH) {//Loop until the user presses the Start button. The user must press the button to start the program. Otherwise it'll loop forever.
+    digitalWrite(LedPin, HIGH);//Turn on the light in the button
+    initDisplay();//Function to show a "loading screen" of sorts on the screen.
 
-          delay(1500);
-        setFreq();
-      }
+    digitalWrite(RelayPin, LOW);//Brings pin tied to the latch on the relay to low
+    delay(20);//waits for 20 ms
+    digitalWrite(RelayPin, HIGH);//Brings pin tied to the relay to high, flipping the latch on the relay
 
-  // save the reading.  Next time through the loop,
-  // it'll be the lastButtonState:
-
+    //delay(1500);
+    Main();//Goes to main function of device
+  }
 }
